2aaa3f7295919aea2b277a36bd252b908d04cae3
Frank <20SunFengshi@bprep.org>
2018-09-11 09:16:42 -0700

    Unit2BLab

---
+++ Unit2BLab/Unit2BLab.py
@@ -0,0 +1,22 @@
+numGrade = int(input("Choose what grade you got"))
+print (numGrade)
+if (numGrade)>=90:
+    print ("Yahoo you got an A")
+elif (numGrade)>=80:
+    print ("Good you got a B")
+elif (numGrade)>=70:
+    print ("Okay you got a C")
+else:
+    print ("You better study harder")
+
+userinput = input ("What's your favorite color?")
+if (userinput)==("blue"):
+    print ("The sky is Blue!")
+elif (userinput)==("green"):
+    print = ("The forest is Green!")
+elif (userinput)==("red"):
+    print ("I love red!")
+elif (userinput)==("yellow"):
+    print ("Yuck yellow!")
+else:
+    print ("I am a dumb computer, I only know 4 colors")
