291c7ca6c48ecfbd104bc4336a54e388a06a6d37
Frank <20SunFengshi@bprep.org>
2018-09-11 09:16:07 -0700

    Unit2ALab

---
+++ Unit2ALab/Unit2ALab.py
@@ -0,0 +1,12 @@
+#print (int(5) + 4.5)
+#print (int(5) + 5)
+#answer1 = input ( )
+#print (int(answer1) + 5)
+i = 100
+j = 200
+x = 300
+y = 100
+print (i==j or i>x)
+print (i<j) and (i>=y)
+print (i==x) and (i==y)
+print (i<j and i<x and i<y)
