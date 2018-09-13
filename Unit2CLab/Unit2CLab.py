32b7bd7f8623e209f8680c3288d781b7be2fbdb8
Frank <20SunFengshi@bprep.org>
2018-09-13 09:18:35 -0700

    Unit2CLab

---
+++ Unit2CLab/Unit2CLab.py
@@ -0,0 +1,20 @@
+numList = [1,2,3,4,5,6,7,8,9,10]
+#print (numList)
+#print (len(numList))
+#print (numList[0])
+#print (numList[9])
+
+#sublist1 = (numList[0:5])
+#print (sublist1)
+#sublist1 = numList + [11]
+#print (sublist1)
+#sublist2 = sublist1.append(12)
+#sublist2 = numList + [13]
+#print (sublist2)
+
+myClasses = ["AP Physics","Calc","Python","Senior Comp","AP Gov"]
+print (myClasses)
+myClasses.remove("Senior Comp")
+print (myClasses)
+favClass = myClasses.pop(2)
+print (favClass)
