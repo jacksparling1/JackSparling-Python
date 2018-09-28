f5c8b21a7557660e38031d569c95178fd4f42e54
Frank <20SunFengshi@bprep.org>
2018-09-28 09:18:51 -0700

    123Lab

--- 123Lab/123Lab.py
+++ 123Lab/123Lab.py
@@ -4,11 +4,12 @@ def main():
     #print (yearinschool(numbGrade))
     longyear = (yearinschool(numbGrade))
     print (longyear)
-    gradesList = [91.5, 88.1, 99.8, 93.4]
-    getNumber= gpa(gradesList)
+    myList = [91.5, 88.1, 99.8, 93.4]
+    getNumber= (gpa(myList))
     print (getNumber)
     theNumber = (letterGrade(getNumber))
     print (theNumber)
+
 
 
 def yearinschool(grade):
@@ -27,18 +28,23 @@ def yearinschool(grade):
 
 def gpa(myList):
     #print (len(myList))
-    results = (myList[0] + myList[1] + myList[2] + myList[3]) / 4
+    if (len(myList))==4:
+        results = (myList[0] + myList[1] + myList[2] + myList[3]) / (len(myList))
+    elif (len(myList))==5:
+        results = (myList[0] + myList[1] + myList[2] + myList[3]) + myList[4] / (len(myList))
+    elif (len(myList))==6:
+        results = (myList[0] + myList[1] + myList[2] + myList[3]) + myList[4] + myList[5] / (len(myList))
     return (results)
 
 def letterGrade(getNumber):
-    if (getNumber)<=90:
-        letter = ("You have an A")
-    elif (getNumber)<=80:
-        letter = ("You have a B")
-    elif (getNumber)<=70:
-        letter = ("You have a C")
-    elif (getNumber)<=60:
-        letter = ("You have a D")
+    if (getNumber)>=90:
+        letter = ("You're getting an A")
+    elif (getNumber)>=80:
+        letter = ("You're getting an B")
+    elif (getNumber)>=70:
+        letter = ("You're getting an C")
+    elif (getNumber)>=60:
+        letter = ("You're getting an D")
     else:
         letter = ("You're failing :(")
     return (letter)
